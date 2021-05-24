b938590b93f28e2b6eb935cfb28be3647d0c6f29
VectorJenkins <55504031+VectorJenkins@users.noreply.github.com>
2021-05-25 02:28:43 +0800

    Add files via upload

---
+++ connection-script.py
@@ -0,0 +1,46 @@
+from urllib.request import urlopen
+import os
+import subprocess
+import time
+
+dir = "C:\speedtest.exe"
+
+
+
+def internet_on():
+   try:
+        response = urlopen('https://www.google.com/', timeout=10)
+        return True
+   except: 
+        return False
+        
+
+#while(internet_on()):
+#    os.startfile(dir)
+#    os.system(dir)
+#    subprocess.Popen([dir])
+#    subprocess.call(dir)
+#    break
+
+flag = 0
+count = 0
+while(True):
+    if internet_on()==True:
+        print(".", end="")
+        time.sleep(1)
+        if flag == 0:
+            time.sleep(3)
+            #os.startfile(dir)
+            #os.system(dir)
+            #subprocess.Popen([dir])
+            subprocess.call(dir)
+            flag = 1
+            
+        else:
+            count = count + 1
+            #print("skipping...")
+            print(str(count), end="")
+            continue
+    else:
+        print("no connection", end+"")
+        flag = 0
